51d279b7378bab432924100e5d037eed7d8c2d44
ishan-singh920 <singhishan920@gmail.com>
2024-05-11 12:09:23 +0530

    commited all files

---
+++ data.py
@@ -0,0 +1,52 @@
+question_data=[
+    {
+        "category":"Science: Computers",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"Digital camera is input device used to take photographs",
+        "correct_answer":"True",
+        "incorrect_answers":[
+            "False"
+        ]
+    },
+    {
+        "category":"Science: Computers",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"FAX stands for First Away Xerox",
+        "correct_answer":"False",
+        "incorrect_answers":[
+            "True"
+        ]
+    },
+    {
+        "category":"Science: Computers",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"IPv6 Internet Protocol address is represented as eight groups of four Octal digits.",
+        "correct_answer":"False",
+        "incorrect_answers":[
+            "True"
+        ]
+    },
+    {
+        "category":"Science: Computers",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"The hexadecimal number system contains digits from 1 - 15.",
+        "correct_answer":"False",
+        "incorrect_answers":[
+            "True"
+        ]
+    },
+    {
+        "category":"Science: Computers",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"MS Word is a hardware.",
+        "correct_answer":"False",
+        "incorrect_answers":[
+            "True"
+        ]
+    }
+]
